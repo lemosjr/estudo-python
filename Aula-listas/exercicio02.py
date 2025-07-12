5b255bad3391f10b2331ed40dc60857b51adc021
Elieuton da silva lemos junior <160804009+lemosjr@users.noreply.github.com>
2025-07-12 00:23:55 -0300

    primeiras aulas e exercicios
    inseridos, arquivos de aula e exercicios

---
+++ Aula-listas/exercicio02.py
@@ -0,0 +1,10 @@
+# 2. Média da Turma
+# --------------------
+# Crie uma lista para armazenar as notas de 5 alunos (por exemplo: [7.5, 8.0, 6.5, 9.0, 7.0]). Calcule e imprima a média dessas notas.
+# * Dica: A média é a soma das notas dividida pelo número de alunos. Use len() para obter o número de alunos.
+
+nota = [7.5, 8.5, 9.5, 5, 4, 3]
+
+media = sum(nota) / len(nota)
+
+print(f"A média das notas é:{media}")
---
+++ Aula-listas/exercicio04.py
@@ -0,0 +1,10 @@
+# --------------------
+# 4. Removendo Duplicatas
+# --------------------
+# Dada a lista numeros = [1, 2, 3, 2, 4, 5, 5, 6]
+# crie um programa que gere uma nova lista contendo apenas os elementos únicos da lista original, sem repetições.
+# A ordem dos elementos na nova lista não importa.
+# * Dica: Considere usar um laço 'for' e verificar se o elemento já existe na nova lista antes de adicioná-lo.
+
+numeros = [1, 2, 3, 2, 4, 5, 5, 6]
+
